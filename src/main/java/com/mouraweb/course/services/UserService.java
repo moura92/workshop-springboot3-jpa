6504445b9da9f3dc68e1eb6e253e0aaa6ca3d478
moura <alisson_moura_rj@hotmail.com>
2023-11-10 14:33:19 -0300

    Exception handling - findById

--- src/main/java/com/mouraweb/course/services/UserService.java
+++ src/main/java/com/mouraweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mouraweb.course.entities.User;
 import com.mouraweb.course.repositories.UserRepository;
+import com.mouraweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	// Retorna o usuario salvo:
---
+++ src/main/java/com/mouraweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.mouraweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not fund. Id " + id);
+	}
+}
